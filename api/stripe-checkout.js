a49eb6263116bdd744ed5b8ed99faac0e97561bd
shishigashira22-lang <shishigashira22@gmail.com>
2026-05-30 22:18:52 +0900

    Update stripe-checkout.js

--- api/stripe-checkout.js
+++ api/stripe-checkout.js
@@ -33,7 +33,9 @@ export default async function handler(req, res) {
         stockName,
         plan,
       }
-    });
+   }, {
+    idempotencyKey: `${stockCode}-${Date.now()}`
+  }); 
 
     res.status(200).json({ url: session.url });
   } catch (e) {
